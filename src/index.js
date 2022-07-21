5b3442b937920dd0d702c9385607499745fdb79b
omarusmani <omarusmani3@hotmail.com>
2022-07-21 07:43:16 -0500

    finished with axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// import axios here
+const { default: Axios } = require("axios")
 
+// import axios here
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
@@ -8,16 +9,20 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+ return Axios(url)
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+    return (Axios(url).then(res=>res.data))
+
   // code here
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  return Axios(url).then(res=>res.status).catch(error=>error.response.status)
   // code here
 }
 
